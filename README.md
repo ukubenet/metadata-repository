470552e5b5eb91240b00db4e5f314a858889288a
dmitry-paltarak <poltdima@gmail.com>
2022-11-06 18:33:01 -0600

    UK-3 create app status page with json response; add http router

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # metadata-repository
 Repository that allows to create, manage, and store metadata
+
+# Running the app in 'dev' mode locally
+* Navigate to the project root in Terminal and run the following to start the server:
+```go
+go run cmd/api/*.go
+```
+* Go to the browser and open Status page
+```go
+http://localhost:4000/status
+```
---
+++ cmd/api/statusHandler.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func (app *application) statusHandler(writer http.ResponseWriter, request *http.Request) {
+	currentStatus := AppStatus{
+		Status:      "Available",
+		Environment: app.config.env,
+		Version:     version,
+	}
+
+	js, err := json.MarshalIndent(currentStatus, "", "\t")
+	if err != nil {
+		app.logger.Println(err)
+	}
+
+	writer.Header().Set("Content-Type", "application/json")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write(js)
+}
