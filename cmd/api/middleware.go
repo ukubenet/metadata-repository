13ae22642af489bed1c0bce7bfc8f2da78c60dea
Michael Kuzmyn <michael.kuzmyn@gmail.com>
2025-01-27 13:01:03 -0600

    initial apphandlers

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -10,9 +10,14 @@ import (
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	allowedHeaders := "Accept, Content-Type, Content-Length, Accept-Encoding, Authorization,X-CSRF-Token"
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", allowedHeaders)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
