7deb09823d94c45bcec60e46cf1f583ba7cbb46a
Michael Kuzmyn <mkuzmyn@hchb.com>
2025-09-26 15:16:43 -0500

    deployer register tests

--- deployer/deployer_test.go
+++ deployer/deployer_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 
 	config "github.com/ukubenet/metadata-repository/config"
+	global "github.com/ukubenet/metadata-repository/global"
 	"github.com/ukubenet/metadata-repository/metadata"
 	metaapi "github.com/ukubenet/metadata-repository/metadata/api"
-	global "github.com/ukubenet/metadata-repository/global"
 )
 
 func TestMain(m *testing.M) {
@@ -42,5 +42,103 @@ func TestDelete(t *testing.T) {
 
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeploy(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.State)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDeploy(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDelete(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.State)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDelete(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeployBalance(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.Balance)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDeploy(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeleteBalance(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.Balance)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDelete(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeployAccumulator(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.Accumulator)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDeploy(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeleteAccumulator(t *testing.T) {
+	register, err := metaapi.ReadRegisterMetadata(metadata.State, "RegisterTest")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	deployer := CreateRegisterFactory(metadata.Accumulator)
+	adapter := deployer.CreateAdapter()
+	err = adapter.RegisterDelete(register)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRegisterDeployError(t *testing.T) {
+	// Test with non-existent register metadata to ensure error handling
+	_, err := metaapi.ReadRegisterMetadata(metadata.State, "NonExistentRegister")
+	if err == nil {
+		t.Fatal("Expected error when reading non-existent register metadata, got nil")
 	}
 }
